9d844369d0eea9c69f98662320bd02f3b0ad102c
bhaktialappanavar <bhaktialappanavar799@gmail.com>
2026-06-30 15:01:03 +0530

    Configure settings for Render

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v)(=eew+13zos(&s(@dj+0_xb*pamh!igw*ub@9r4e$6egbah*'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-v)(=eew+13zos(&s(@dj+0_xb*pamh!igw*ub@9r4e$6egbah*"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
@@ -121,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "dashboard"
